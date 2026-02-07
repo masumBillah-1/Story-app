2991f1631a2d7c70955f7bdb477304f2463c5f33
akhiakter25556 <akhiakterherpower80@gmail.com>
2026-02-07 22:30:45 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-=======
 
->>>>>>> 20e5a31aa862fa555b5b2d9603805d97a21b796d
 import Homepage from './pages/Homepage.jsx'
 import PublicStory from './components/PublicStory.jsx'
 createRoot(document.getElementById('root')).render(
