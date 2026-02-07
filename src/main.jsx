9b192f953131e70608425f0c26db49801aa2c5a2
akhiakter25556 <akhiakterherpower80@gmail.com>
2026-02-07 20:56:16 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Homepage from './pages/Homepage.jsx'
-
+import PublicStory from './components/PublicStory.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Homepage/>
+    <Homepage />
+<PublicStory />
+
+
   </StrictMode>,
 )
